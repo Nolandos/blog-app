6e19b5503fa2e3e675cc033afc4c1f2dbd8d67f5
Łukasz Nolka <nolkalukasz@gmail.com>
2019-09-12 11:12:40 +0200

    Update config for client

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 export const API_URL = '/api';
 export const BASE_URL = 'https://blog-mern-app.herokuapp.com/';
-export const FACEBOOK_API_ID = '391940735073188';
+export const FACEBOOK_API_ID = '391940735073188'; 
