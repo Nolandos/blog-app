78464ec468f0eb4924655e6f471581aa45930873
Łukasz Nolka <nolkalukasz@gmail.com>
2019-09-12 09:01:22 +0200

    connect app with MONGO atlas

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    PORT: 8000,
-    DB: 'mongodb://localhost/mernapp',
+    PORT: process.env.PORT || 8000,
+    DB: 'mongodb+srv://lukasz-nolka:testuser123@cluster0-zou0m.mongodb.net/test?retryWrites=true&w=majority',
 };
