996af711b2a668040f6539dbf5d95cbff33dd0d8
Łukasz Nolka <nolkalukasz@gmail.com>
2019-09-15 09:59:22 +0200

    change config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-export const API_URL = process.env.API_URL;
+export const API_URL = '/api';
 export const BASE_URL = process.env.BASE_URL;
 export const FACEBOOK_API_ID = process.env.FACEBOOK_API_ID;
