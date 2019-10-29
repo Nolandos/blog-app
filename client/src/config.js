b8f18926f05bcb2f9e311aee10feb161fa058759
Łukasz Nolka <nolkalukasz@gmail.com>
2019-10-29 16:05:00 +0100

    Refactor client code

--- client/src/config.js
+++
@@ -1,2 +0,0 @@
-export const API_URL = '/api';
-export const BASE_URL = 'https://blog-mern-app.herokuapp.com';
--- server/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 8000,
-    DB: 'mongodb+srv://lukasz-nolka:testuser123@cluster0-zou0m.mongodb.net/test?retryWrites=true&w=majority'
-};
